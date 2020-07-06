2a1975cf860e061919fd635f2cbc788678d43042
Duarte Carrilho da Graça <duarte@duarte.gd>
2020-07-06 11:15:52 +0200

    start structuring html in file

--- atoms/default/client/js/main.js
+++ atoms/default/client/js/main.js
@@ -1,22 +1,51 @@
-var el = document.createElement('script');
-el.src = '<%= atomPath %>/app.js';
-document.body.appendChild(el);
+// var el = document.createElement('script');
+// el.src = '<%= atomPath %>/app.js';
+// document.body.appendChild(el);
+
+startInteractive();
+
+function startInteractive() {
+  let int;
+  int = setInterval(() => {
+    let keyEl = document.querySelector('.after-the-fires__wrapper');
+    if (keyEl) {
+      clearInterval(int);
+
+      setTimeout(() => {
+        shapeDom();
+
+      }, 5000)
+
+    }
+  }, 10)
+}
 
 
+function shapeDom() {
+  const interactiveBase = document.querySelector('.after-the-fires__wrapper');
 
-setTimeout(() => {
-  if (window.resize) {  
-    const html = document.querySelector('html')
-    const body = document.querySelector('body')
-  
-    html.style.overflow = 'hidden'
-    html.style.margin = '0px'
-    html.style.padding = '0px'
-  
-    body.style.overflow = 'hidden'
-    body.style.margin = '0px'
-    body.style.padding = '0px'
-  
-  window.resize()
-  }
-},100)
+  const interactiveRoot = interactiveBase.parentElement.parentElement;
+  const endEl = document.querySelector('.l-footer.u-cf');
+  const articleRoot = interactiveRoot.parentElement;
+
+  let start = false,
+    end = false;
+
+  articleRoot.childNodes.forEach((el) => {
+    console.log(el);
+
+    if (el === endEl) {
+      end = true;
+    } else if (!end && start) {
+      // Add this element
+      interactiveBase.appendChild(el);
+
+    } else if (el === interactiveRoot) {
+      start = true;
+    }
+  })
+  // // go up to find the article's root
+  // const articleRoot = i.parentElement.parentElement.parentElement;
+
+  // console.log(articleRoot, 'r');
+}
